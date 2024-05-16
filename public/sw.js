d89fb0e0f831909309045ae1f3f79ad02544a624
Yeojinny <pdg03092@gmail.com>
2024-05-16 17:55:34 +0900

    chore: gitognore 파일 수정

--- public/sw.js
+++ public/sw.js
@@ -1,304 +1,94 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
 if (!self.define) {
-  let e,
-    s = {}
-  const a = (a, n) => (
-    (a = new URL(a + '.js', n).href),
-    s[a] ||
-      new Promise((s) => {
-        if ('document' in self) {
-          const e = document.createElement('script')
-          ;(e.src = a), (e.onload = s), document.head.appendChild(e)
-        } else (e = a), importScripts(a), s()
-      }).then(() => {
-        let e = s[a]
-        if (!e) throw new Error(`Module ${a} didn’t register its module`)
-        return e
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
       })
-  )
-  self.define = (n, i) => {
-    const t =
-      e ||
-      ('document' in self ? document.currentScript.src : '') ||
-      location.href
-    if (s[t]) return
-    let c = {}
-    const r = (e) => a(e, t),
-      o = { module: { uri: t }, exports: c, require: r }
-    s[t] = Promise.all(n.map((e) => o[e] || r(e))).then((e) => (i(...e), c))
-  }
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
 }
-define(['./workbox-c06b064f'], function (e) {
-  'use strict'
-  importScripts(),
-    self.skipWaiting(),
-    e.clientsClaim(),
-    e.precacheAndRoute(
-      [
-        {
-          url: '/_next/static/0kvuKd7lcPaiEs-7D3lv2/_buildManifest.js',
-          revision: '2ec694eb52ae4f523f265a46bae4d768',
-        },
-        {
-          url: '/_next/static/0kvuKd7lcPaiEs-7D3lv2/_ssgManifest.js',
-          revision: 'b6652df95db52feb4daf4eca35380933',
-        },
-        {
-          url: '/_next/static/chunks/fd9d1056-2821b0f0cabcd8bd.js',
-          revision: '0kvuKd7lcPaiEs-7D3lv2',
-        },
-        {
-          url: '/_next/static/chunks/framework-f66176bb897dc684.js',
-          revision: '0kvuKd7lcPaiEs-7D3lv2',
-        },
-        {
-          url: '/_next/static/chunks/main-49106394d9f6a4a7.js',
-          revision: '0kvuKd7lcPaiEs-7D3lv2',
-        },
-        {
-          url: '/_next/static/chunks/main-app-623b80217ed6abc2.js',
-          revision: '0kvuKd7lcPaiEs-7D3lv2',
-        },
-        {
-          url: '/_next/static/chunks/pages/_app-6a626577ffa902a4.js',
-          revision: '0kvuKd7lcPaiEs-7D3lv2',
-        },
-        {
-          url: '/_next/static/chunks/pages/_error-1be831200e60c5c0.js',
-          revision: '0kvuKd7lcPaiEs-7D3lv2',
-        },
-        {
-          url: '/_next/static/chunks/polyfills-78c92fac7aa8fdd8.js',
-          revision: '79330112775102f91e1010318bae2bd3',
-        },
-        {
-          url: '/_next/static/chunks/webpack-1b94650512334e97.js',
-          revision: '0kvuKd7lcPaiEs-7D3lv2',
-        },
-        {
-          url: '/icon-192x192.png',
-          revision: '0846a77b2550492e534f7a1a7012eee8',
-        },
-        {
-          url: '/icon-256x256.png',
-          revision: 'bac2fc762e53b57f2ce55e628e5c5be9',
-        },
-        {
-          url: '/icon-384x384.png',
-          revision: 'e9d31a292c162deb204ee96297648157',
-        },
-        {
-          url: '/icon-512x512.png',
-          revision: '85feb3a273be45d763ea33b13ef941c8',
-        },
-        {
-          url: '/images/mfc-logo.png',
-          revision: '1509ef459129bc4ce20097d00fcdba46',
-        },
-        { url: '/manifest.json', revision: '49377dff528b9ba7c1a926a3f5242dd3' },
-        { url: '/next.svg', revision: '8e061864f388b47f33a1c3780831193e' },
-        { url: '/vercel.svg', revision: '61c6b19abff40ea7acd577be818f3976' },
-      ],
-      { ignoreURLParametersMatching: [/^utm_/, /^fbclid$/] },
-    ),
-    e.cleanupOutdatedCaches(),
-    e.registerRoute(
-      '/',
-      new e.NetworkFirst({
-        cacheName: 'start-url',
-        plugins: [
-          {
-            cacheWillUpdate: async ({ response: e }) =>
-              e && 'opaqueredirect' === e.type
-                ? new Response(e.body, {
-                    status: 200,
-                    statusText: 'OK',
-                    headers: e.headers,
-                  })
-                : e,
-          },
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
-      new e.CacheFirst({
-        cacheName: 'google-fonts-webfonts',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 4, maxAgeSeconds: 31536e3 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
-      new e.StaleWhileRevalidate({
-        cacheName: 'google-fonts-stylesheets',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 4, maxAgeSeconds: 604800 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-      new e.StaleWhileRevalidate({
-        cacheName: 'static-font-assets',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 4, maxAgeSeconds: 604800 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-      new e.StaleWhileRevalidate({
-        cacheName: 'static-image-assets',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 64, maxAgeSeconds: 2592e3 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\/_next\/static.+\.js$/i,
-      new e.CacheFirst({
-        cacheName: 'next-static-js-assets',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 64, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\/_next\/image\?url=.+$/i,
-      new e.StaleWhileRevalidate({
-        cacheName: 'next-image',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 64, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\.(?:mp3|wav|ogg)$/i,
-      new e.CacheFirst({
-        cacheName: 'static-audio-assets',
-        plugins: [
-          new e.RangeRequestsPlugin(),
-          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\.(?:mp4|webm)$/i,
-      new e.CacheFirst({
-        cacheName: 'static-video-assets',
-        plugins: [
-          new e.RangeRequestsPlugin(),
-          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\.(?:js)$/i,
-      new e.StaleWhileRevalidate({
-        cacheName: 'static-js-assets',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 48, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\.(?:css|less)$/i,
-      new e.StaleWhileRevalidate({
-        cacheName: 'static-style-assets',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\/_next\/data\/.+\/.+\.json$/i,
-      new e.StaleWhileRevalidate({
-        cacheName: 'next-data',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      /\.(?:json|xml|csv)$/i,
-      new e.NetworkFirst({
-        cacheName: 'static-data-assets',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      ({ sameOrigin: e, url: { pathname: s } }) =>
-        !(!e || s.startsWith('/api/auth/callback') || !s.startsWith('/api/')),
-      new e.NetworkFirst({
-        cacheName: 'apis',
-        networkTimeoutSeconds: 10,
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 16, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      ({ request: e, url: { pathname: s }, sameOrigin: a }) =>
-        '1' === e.headers.get('RSC') &&
-        '1' === e.headers.get('Next-Router-Prefetch') &&
-        a &&
-        !s.startsWith('/api/'),
-      new e.NetworkFirst({
-        cacheName: 'pages-rsc-prefetch',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      ({ request: e, url: { pathname: s }, sameOrigin: a }) =>
-        '1' === e.headers.get('RSC') && a && !s.startsWith('/api/'),
-      new e.NetworkFirst({
-        cacheName: 'pages-rsc',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      ({ url: { pathname: e }, sameOrigin: s }) => s && !e.startsWith('/api/'),
-      new e.NetworkFirst({
-        cacheName: 'pages',
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 86400 }),
-        ],
-      }),
-      'GET',
-    ),
-    e.registerRoute(
-      ({ sameOrigin: e }) => !e,
-      new e.NetworkFirst({
-        cacheName: 'cross-origin',
-        networkTimeoutSeconds: 10,
-        plugins: [
-          new e.ExpirationPlugin({ maxEntries: 32, maxAgeSeconds: 3600 }),
-        ],
-      }),
-      'GET',
-    ),
-    (self.__WB_DISABLE_DEV_LOGS = !0)
-})
+define(['./workbox-7144475a'], (function (workbox) { 'use strict';
+
+  importScripts();
+  self.skipWaiting();
+  workbox.clientsClaim();
+  workbox.registerRoute("/", new workbox.NetworkFirst({
+    "cacheName": "start-url",
+    plugins: [{
+      cacheWillUpdate: async ({
+        response: e
+      }) => e && "opaqueredirect" === e.type ? new Response(e.body, {
+        status: 200,
+        statusText: "OK",
+        headers: e.headers
+      }) : e
+    }]
+  }), 'GET');
+  workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
+    "cacheName": "dev",
+    plugins: []
+  }), 'GET');
+  self.__WB_DISABLE_DEV_LOGS = true;
+
+}));
+//# sourceMappingURL=sw.js.map
