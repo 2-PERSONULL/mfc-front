3d14d433259e8a8edd7e6102e7eacbfc3cd2c3e6
Yeojinny <pdg03092@gmail.coom>
2024-05-27 00:08:33 +0900

    feat: 파트너 프로필 채팅가능시간 및 타임피커 컴포넌트 추가

--- src/components/pages/partner/mypage/profile/PartnerChatTime.tsx
+++ src/components/pages/partner/mypage/profile/PartnerChatTime.tsx
@@ -4,12 +4,24 @@ import React, { useState } from 'react'
 import PartnerProfileTitleAndEdit from '@/components/pages/partner/mypage/profile/PartnerProfileTitleAndEdit'
 import Modal from '@/components/common/Modal'
 import StretchedRoundedButton from '@/components/ui/button/StretchedRoundedButton'
+import TimepickerSwiper from '@/components/ui/picker/TimepickerSwiper'
+import useToast from '@/stores/toast'
 
 export default function PartnerChatTime({ chatTime }: { chatTime: string }) {
+  const [startTime, setStartTime] = useState<number>(0)
+  const [endTime, setEndTime] = useState<number>(1)
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+  const { showToast } = useToast()
 
   const saveHandler = () => {
     // fetch로 데이터 저장
+    if (startTime === endTime) {
+      showToast({
+        content: '시작 시간과 종료 시간이 같습니다.',
+        type: 'warning',
+      })
+      return
+    }
     setIsModalOpen(false)
   }
 
@@ -20,12 +32,16 @@ export default function PartnerChatTime({ chatTime }: { chatTime: string }) {
   return (
     <div>
       {isModalOpen && (
-        <Modal title="한줄소개" closeModal={() => setIsModalOpen(false)}>
+        <Modal title="채팅 가능 시간" closeModal={() => setIsModalOpen(false)}>
           <div>
             <h1 className="font-semibold">
               파트너님의 연락 가능한 시간을 알려주세요.
             </h1>
-            <div className="relative border border-[#b6b6b6] bg-[#fff] rounded-[4px] mb-[28px] overflow-visible mt-3" />
+            <div className="flex items-center justify-center gap-2 mt-10">
+              <TimepickerSwiper hour={startTime} setHour={setStartTime} />
+              ~
+              <TimepickerSwiper hour={endTime} setHour={setEndTime} />
+            </div>
             <div className="fixed bottom-7 w-full left-0 right-0 px-6">
               <StretchedRoundedButton
                 comment="저장"
@@ -41,9 +57,11 @@ export default function PartnerChatTime({ chatTime }: { chatTime: string }) {
         // 데이터 있는지 여부
         isEmpty={!chatTime}
         content={
-          <p className="text-[14px]">
-            {chatTime || ' 파트너님의 연락 가능한 시간을 알려주세요.'}
-          </p>
+          <div>
+            <p className="text-[14px]">
+              {chatTime || ' 파트너님의 연락 가능한 시간을 알려주세요.'}
+            </p>
+          </div>
         }
       />
     </div>
---
+++ src/types/partnerProfileTypes.ts
@@ -0,0 +1,4 @@
+export interface ChatTimeType {
+  label: string
+  value: number
+}
