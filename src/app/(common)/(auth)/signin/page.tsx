ce8b821611c9a7fbd584fec6c128cd33535cb524
hsc03 <hsc0710@naver.com>
2024-05-31 22:51:57 +0900

    feat: 로그인 페이지 퍼블리싱 완료

--- src/app/(common)/(auth)/signin/page.tsx
+++ src/app/(common)/(auth)/signin/page.tsx
@@ -1,10 +1,20 @@
 import { Suspense } from 'react'
-import LoginForm from '@/components/pages/auth/signIn/LoginForm'
+import SignInLogo from '@/components/pages/auth/signIn/SignInLogo'
+import SignInForm from '@/components/pages/auth/signIn/SignInForm'
+import SignInAddOn from '@/components/pages/auth/signIn/SignInAddOn'
+import SNSSignInButton from '@/components/pages/auth/SNSSignInButton'
+import LicenseInfo from '@/components/common/LicenseInfo'
 
 export default function SignIn() {
   return (
-    <Suspense>
-      <LoginForm />
-    </Suspense>
+    <div className="bg-white min-h-screen w-full flex flex-col items-center px-6">
+      <SignInLogo />
+      <Suspense>
+        <SignInForm />
+      </Suspense>
+      <SignInAddOn />
+      <SNSSignInButton />
+      <LicenseInfo />
+    </div>
   )
 }
---
+++ src/components/pages/auth/signIn/SignInLogo.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+
+export default function SignInLogo() {
+  return (
+    <div className="flex flex-col items-center gap-2 bg-white w-full mb-14 mt-[45%]">
+      <Image
+        src="https://personull-bucket.s3.ap-northeast-2.amazonaws.com/logo/signInLogo.svg"
+        alt="signInLogo"
+        priority
+        width={0}
+        height={0}
+        style={{ width: 'auto', height: 'auto' }}
+      />
+      <span className="font-black text-xl">LOGIN</span>
+    </div>
+  )
+}
