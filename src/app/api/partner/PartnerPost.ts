7790416d794cc9a7e24981834ef561bf01fce2e9
Yeojinny <pdg03092@gmail.coom>
2024-06-02 17:37:34 +0900

    feat: 파트너 스타일 업로드 fetch

---
+++ src/app/api/partner/PartnerPost.ts
@@ -0,0 +1,33 @@
+'use server'
+
+import getFetchHeader from '@/utils/getFetchHeader'
+
+export default async function addPartnerPost(
+  imageUrl: string | null,
+  tags: string[],
+) {
+  try {
+    const header = await getFetchHeader()
+
+    if (!header) {
+      console.log('session not found')
+      return null
+    }
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/sns-service/posts`,
+      {
+        method: 'POST',
+        headers: header,
+        body: JSON.stringify({ imageUrl, tags }),
+      },
+    )
+
+    const data = await response.json()
+    if (!data.isSuccess) console.log('add post error:', data)
+    return null
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
--- src/components/pages/partner/mypage/style/StyleEditor.tsx
+++ src/components/pages/partner/mypage/style/StyleEditor.tsx
@@ -1,16 +1,27 @@
 'use client'
 
 import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
 import UploadStyle from '@/components/pages/partner/mypage/style/UploadStyle'
 import StyleTagEditor from '@/components/pages/partner/mypage/style/StyleTagEditor'
+import addPartnerPost from '@/app/api/partner/PartnerPost'
+import useToast from '@/stores/toast'
 
 export default function StyleEditor() {
+  const router = useRouter()
+  const { showToast } = useToast()
   const [image, setImage] = useState<string | null>('')
   const [tags, setTags] = useState<string[]>([])
 
   const uploadStyle = () => {
-    // fetch
-    console.log(image, tags)
+    if (!image) {
+      showToast({ content: '이미지를 업로드해주세요.', type: 'warning' })
+      return
+    }
+
+    addPartnerPost(image, tags)
+    showToast({ content: '스타일이 업로드되었습니다.', type: 'success' })
+    router.replace('/partner/mypage')
   }
 
   return (
