68fc12ae050a22d1150043eb4f21fd7998107be5
Yeojinny <pdg03092@gmail.com>
2024-06-17 12:03:16 +0900

    fix: 결제 이후 페이지 오류 수정 suspense 추가

--- src/components/pages/user/payments/PaymentProcess.tsx
+++ src/components/pages/user/payments/PaymentProcess.tsx
@@ -1,23 +1,19 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { checkPayment } from '@/actions/user/Payments'
 import LoadingModal from '@/components/common/LoadingModal'
 import useToast from '@/stores/toast'
 
-export default function PaymentProcess({
-  paymentId,
-  value,
-  callback,
-}: {
-  paymentId: string
-  value: string
-  callback: string
-}) {
+export default function PaymentProcess() {
   const { showToast } = useToast()
   const router = useRouter()
   const [payStatus, setPayStatus] = useState<number | null>(null)
+  const searchParams = useSearchParams()
+  const paymentId = searchParams.get('paymentId')
+  const value = searchParams.get('value')
+  const callbackUrl = searchParams.get('callback')
 
   // 주문 데이터의 가격과 실제 지불된 금액을 비교합니다.
   useEffect(() => {
@@ -39,7 +35,7 @@ export default function PaymentProcess({
       showToast({ content: '결제에 실패했습니다.', type: 'error' })
     }
 
-    router.replace(`${callback}`)
+    router.replace(`${callbackUrl}`)
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [payStatus])
