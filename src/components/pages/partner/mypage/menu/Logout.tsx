358223d18b1bd422dfebeb8780eaa76bd7b452d5
Yeojinny <pdg03092@gmail.coom>
2024-06-23 22:17:34 +0900

    fix: 유저에서 파트너 첫 전환 시 프로필 오류 수정

--- src/components/pages/partner/mypage/menu/Logout.tsx
+++ src/components/pages/partner/mypage/menu/Logout.tsx
@@ -6,12 +6,12 @@ import { signOut } from 'next-auth/react'
 
 export default function Logout() {
   return (
-    <section className="border flex border-gray-200 rounded-[6px] mx-5 mt-3 h-[70px] px-4">
-      <button
-        type="button"
-        onClick={() => signOut({ callbackUrl: '/' })}
-        className="flex items-center justify-around"
-      >
+    <section
+      role="presentation"
+      onClick={() => signOut({ callbackUrl: '/' })}
+      className="border flex border-gray-200 rounded-[6px] mx-5 mt-3 h-[70px] px-4"
+    >
+      <div className="flex items-center justify-around">
         <Image
           src="https://personull-bucket.s3.ap-northeast-2.amazonaws.com/icon/exit.svg"
           alt="logout"
@@ -20,7 +20,7 @@ export default function Logout() {
           className="mr-4"
         />
         <p>로그아웃</p>
-      </button>
+      </div>
     </section>
   )
 }
