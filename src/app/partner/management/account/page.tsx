e2832f6d3f5c5a7d26f83f86590aaf95ada7e8b3
Yeojinny <pdg03092@gmail.com>
2024-07-04 13:47:14 +0900

    fix: 유저 계정관리 준비중 컴포넌트 추가

--- src/app/partner/management/account/page.tsx
+++ src/app/partner/management/account/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import UnderConstruction from '@/components/common/UnderConstruction'
 
 export default function PartnerAccountPage() {
-  return <div>PartnerAccountPage</div>
+  return <UnderConstruction />
 }
